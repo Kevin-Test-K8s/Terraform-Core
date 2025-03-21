2eedff53f3b831b95e618eff1ee7dfe221e08995
kevden01 <kevinklein@mac.com>
2025-03-21 12:25:34 +0100

    replaced tfvars/variables.tf with locals.tf

---
+++ locals.tf
@@ -0,0 +1,29 @@
+locals {
+  config = yamldecode(file("${path.module}/variables.yml"))
+}
+
+# Variables extraídas del YAML
+variable "resource_group_name" {
+  type    = string
+  default = local.config.resource_group_name
+}
+
+variable "cluster_name" {
+  type    = string
+  default = local.config.cluster_name
+}
+
+variable "location" {
+  type    = string
+  default = local.config.location
+}
+
+variable "subscription_id" {
+  type    = string
+  default = local.config.subscription_id
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = local.config.storage_account_name
+}
--- terraform.tfvars
+++
@@ -1,5 +0,0 @@
-resource_group_name     = "${yamldecode(file("variables.yml")).resource_group_name}"
-cluster_name            = "${yamldecode(file("variables.yml")).cluster_name}"
-location                = "${yamldecode(file("variables.yml")).location}"
-subscription_id         = "${yamldecode(file("variables.yml")).subscription_id}"
-storage_account_name    = "${yamldecode(file("variables.yml")).storage_account_name}"

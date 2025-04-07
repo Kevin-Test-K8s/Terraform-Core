418b9967885b8f83edf82435f5115c23779a6cc6
kevden01 <kevinklein@mac.com>
2025-04-07 10:43:21 +0200

    correction cluster user role

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "azurerm_storage_container" "terraform" {
 
 resource "azurerm_role_assignment" "aks_user" {
   scope                = azurerm_kubernetes_cluster.aks.id
-  role_definition_name = "Azure Kubernetes Service Cluster User"
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
   principal_id         = local.config.github_actions_object_id
 }
 
